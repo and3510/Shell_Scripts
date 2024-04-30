3cc23ab4ade74b3daadc9792c625220fa2ef9a51
and3510 <and.dovale@gmail.com>
2024-04-30 11:02:25 -0300

    upgrade files

--- git/git-remote.sh
+++ git/git-remote.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Author: Anderson do Vale 
+# Description: This script will help you to add link-remote as
+# input without typing before "git remote add origin".
+# Version: 1.0
+
+
 
 echo "git remote add origin <link>"
 read -p "Enter link: " link
--- git/push-f.sh
+++ git/push-f.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Author: Anderson do Vale 
+# Description: This script will help you to use <git push <link> main -f>,
+# but just typing link-remote.
+# Version: 1.0
+
 echo "git push <link> main -f"
 read -p "Enter link: " link
 
