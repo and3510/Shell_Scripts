c9c9a70e0b1ad4de32dfa7bd132cd602e5770032
and3510 <and.dovale@gmail.com>
2024-05-13 13:39:01 -0300

    update check-git

---
+++ git-scripts/check_gits.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Encontre GIT!"
+read -p "Digite o caminho: " caminho
+
+var=$(date +%d-%m-%Y)
+
+registro=$(find $caminho -name .git | wc -l )
+
+echo "O numero de gits nesse caminho $caminho na data $var foi $registro " >> /home/anderson/.config/Code/check_gits.log
+
+echo "--------------------------"
+
+find $caminho -name .git
+clear
+echo "O numero de gits nesse caminho $caminho na data $var foi $registro "
+
+
+
